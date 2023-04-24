e7f42cea9430c590f2beef1607666d0214ce72ba
Yug376 <118145447+Yugo376@users.noreply.github.com>
2023-04-24 03:30:29 +0000

    [add]kadai2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,35 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(list_params)
+    book.save
+    flash[:notice]="投稿が成功しました"
+
+    redirect_to book_path(book.id)
+  end
+
+  def show
+    @book=Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get '/index' => 'books#index'
+  post '/index' => 'books#create'
+  get 'index/:id' => 'books#show' , as: 'book'
+  get 'index/:id/edit' => 'books#edit', as: 'book_edit'
+  patch 'index/:id' => 'books#update' , as: 'book_update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
