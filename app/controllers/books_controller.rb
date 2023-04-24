2bae5647aacb9e7882552d16392a532ce4005263
Yug376 <118145447+Yugo376@users.noreply.github.com>
2023-04-24 06:22:56 +0000

    [finish] kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,17 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def create
-    book = Book.new(list_params)
-    book.save
-    flash[:notice]="投稿が成功しました"
 
-    redirect_to book_path(book.id)
+  def create
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice]="Book was successfully created"
+      redirect_to book_path(book.id)
+    else
+      @book = book
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -19,11 +24,23 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice]="Book was successfully updated"
+      redirect_to book_path(book.id)
+    else 
+      @book=book
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice]="Book was successfully destroyed"
+    redirect_to '/books'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get '/index' => 'books#index'
-  post '/index' => 'books#create'
-  get 'index/:id' => 'books#show' , as: 'book'
-  get 'index/:id/edit' => 'books#edit', as: 'book_edit'
-  patch 'index/:id' => 'books#update' , as: 'book_update'
+  get '/books' => 'books#index'
+  post '/books' => 'books#create'
+  get 'books/:id' => 'books#show' , as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id/edit' => 'books#update' , as: 'book_update'
+  delete 'books/:id' => 'books#destroy', as: 'book_destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
